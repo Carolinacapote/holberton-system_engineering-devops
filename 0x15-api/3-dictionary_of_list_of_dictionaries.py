09585ccc626bdcbfcbe335c6c63f8811a84085ff
Carolinacapote <carolina.capote28@gmail.com>
2021-09-14 02:41:07 +0000

    [ADD]Display list of all users

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Python script that, using a REST API, for a given employee ID, returns
+information about his/her TODO list progress.
+"""
+import json
+import requests
+
+
+if __name__ == "__main__":
+    api_url = 'https://jsonplaceholder.typicode.com'
+    # Getting all users info
+    response = requests.get(api_url + '/users')
+    users_info = response.json()
+
+    # iterating into users list and saving in dictionary
+    users_dict = {}
+    new_list = []
+    for user in users_info:
+        response = requests.get(api_url + '/users/{}/todos'.
+                                format(user.get('id')))
+        todo_list = response.json()
+        for task in todo_list:
+            task_dict = {'username': user.get('username'),
+                         'task': task.get('title'),
+                         'completed': task.get('completed')}
+
+            new_list.append(task_dict)
+
+        users_dict[user.get('id')] = new_list
+
+    with open('todo_all_employees.json', 'w') as new_file:
+        json.dump(users_dict, new_file)
